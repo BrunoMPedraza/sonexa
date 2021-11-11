dff975b39ddbbeeda063dcb8ea1c3a350280d611
BrunoMPedraza <61323575+BrunoMPedraza@users.noreply.github.com>
2021-11-11 16:14:08 -0300

    Business People Part
    Second part of Home
    Also removed clumsiness

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,17 +2,25 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <div className="content flow">
-            <div className="title">
-                <div className="body-Home">
-                    <div className="col"></div>
-                    <div className="col">
-                        <img className='girlOnPc' src={require('../images/illustration/webp.webp').default} alt="Logo: Woman with cyan background in shield" />
-                        <img className='separatorOne' src={require('../images/separator.svg').default} alt="separation line" /></div>
+        <main>
+            <div className="home-One">
+                <div className="col phrase">We bring products to the <b>top</b></div>
+                <div className="col">
+                    <img className='girlOnPc' src={require('../images/illustration/webp.webp').default} alt="Logo: Woman with cyan background in shield" />
+                    <img className='separatorOne' src={require('../images/separator.svg').default} alt="separation line" />
                 </div>
             </div>
-            Contact
-        </div>
+            <div className="home-Second">
+                <div className="col">
+                    <img className='businessPpl' src={require('../images/illustration/meet.webp').default} alt="Illustration of three persons of different genders planning over some papework" />
+                    <img className='separatorOne' src={require('../images/separator.svg').default} alt="separation line" />
+                </div>
+                <div className="col phrase longTxt">with the use of tecniques based
+                    on a big-data scale, we take the
+                    form of thought of user logs
+                    as info that we use to boost your company</div>
+            </div>
+        </main>
     )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,7 @@ import Separator from '../images/separator.svg'
 const Home = () => {
     return (
         <main class="home">
-            <div class="fContent">
-                <img className='girlOnPc' src={require('../images/illustration/webp.webp').default} alt="Logo: Woman with cyan background in shield"/>
-                <img className='separatorOne'src={Separator} alt="separation line" />
-            </div>
+            
         </main>
     )
 }
