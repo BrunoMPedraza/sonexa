891e49422521c16733904d26fc33bc9fe790db99
BrunoMPedraza <61323575+BrunoMPedraza@users.noreply.github.com>
2021-11-14 18:07:32 -0300

    Dark mode
    Finished

--- src/components/DarkMode.js
+++ src/components/DarkMode.js
@@ -1,41 +1,41 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {SiDarkreader} from 'react-icons/si'
 
 const DarkMode = () => {
-
-//     --primary-bg:#DEE8FD;
-//   --secondary-bg:#455BAF;
-//   --text-bg:#C6E6F5;
-//   --white-txt:rgb(255, 255, 255);
-//   --black-txt:rgb(0, 0, 0);
-
-//   /* Fonts */
-//   /* Font Family */
-//   --ff-common:'Rokkitt', serif;
-//   /*Font Size */
-//   --fs-title:3rem;
-//   --fs-lg: 2.2rem;
-//   --fs-sm: .8rem;
-//   /* Person size */
-//   /* These illustrations are based on people, so it makes sense for me to respect a proportion between each other */
-//   --ps-common:60%;
-//   --ps-device:100%;
+    const [dark,setDark] = useState(false)
 
 
-    const colorChange = (e) =>{
-        console.log(document.documentElement.style.getPropertyValue('--primary-bg'));
-        document.documentElement.style.setProperty('--global-bg','#373737')
-        document.documentElement.style.setProperty('--primary-bg','#373737')
-        document.documentElement.style.setProperty('--secondary-bg','#2F3E76')
-        document.documentElement.style.setProperty('--text-bg','#5F6D74')
-        document.documentElement.style.setProperty('--svg-filter','0.4')
-        console.log(document.documentElement.style.getPropertyValue('--svg-filter'));
+
+    // --global-bg:white;
+    // --primary-bg:#DEE8FD;
+    // --secondary-bg:#455BAF;
+    // --text-bg:#C6E6F5;
+    // --white-txt:rgb(255, 255, 255);
+    // --black-txt:rgb(0, 0, 0);
+    // --svg-filter:1;
+
+    const colorChange = () =>{
+        setDark(!dark);
+        if(dark){
+            document.documentElement.style.setProperty('--global-bg','white')
+            document.documentElement.style.setProperty('--primary-bg','#DEE8FD')
+            document.documentElement.style.setProperty('--secondary-bg','#455BAF')
+            document.documentElement.style.setProperty('--text-bg','#C6E6F5')
+            document.documentElement.style.setProperty('--svg-filter','1')
+            document.documentElement.style.setProperty('--black-txt','black')
+        }else{
+            document.documentElement.style.setProperty('--global-bg','#373737')
+            document.documentElement.style.setProperty('--primary-bg','#373737')
+            document.documentElement.style.setProperty('--secondary-bg','#2F3E76')
+            document.documentElement.style.setProperty('--text-bg','#5F6D74')
+            document.documentElement.style.setProperty('--svg-filter','0.4')
+            document.documentElement.style.setProperty('--black-txt','#FFFFFF')}
     }
 
 
     return (
         <>
-            <SiDarkreader className='darkMode' id='dark' onClick={(e)=>colorChange(e)}/>
+            <SiDarkreader className='darkMode' id='dark' onClick={(e)=>colorChange()}/>
         </>
     )
 }
